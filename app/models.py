24fddf59802183eb9b5dc6221b31f731d7d94849
Tedu <450358108@qq.com>
2019-10-21 21:13:12 +0800

    second commit

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 
+
 # 会员
 class User(db.Model):
     __tablename__ = "user"
@@ -28,6 +29,8 @@ class User(db.Model):
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 注册时间
     uuid = db.Column(db.String(255), unique=True)  # 唯一标志符
     userlogs = db.relationship('Userlog', backref='user')  # 会员日志外键关系
+    comment = db.relationship('Comment', backref='user')  # 评论外键关系关联
+    moviecols = db.relationship('Moviecol', backref='user')  # 收藏外键关系关联
 
     def __repr__(self):
         return "<User %r>" % self.name
@@ -43,3 +46,106 @@ class Userlog(db.Model):
 
     def __repr__(self):
         return "<Userlog %r>" % self.id
+
+
+# 标签
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), unique=True)  # 标题
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    movies = db.relationship('Movie', backref='tag')  # 电影外键关系关联
+
+    def __repr__(self):
+        return "<Tag %r>" % self.name
+
+
+# 电影
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    url = db.Column(db.String(255), unique=True)  # 地址
+    info = db.Column(db.Text)  # 简介
+    logo = db.Column(db.String(255), unique=True)  # 封面
+    star = db.Column(db.SmallInteger)  # 星级
+    playnum = db.Column(db.BigInteger)  # 播放量
+    commentnum = db.Column(db.BigInteger)  # 评论量
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))  # 所属标签
+    area = db.Column(db.String(255))  # 上映地区
+    release_time = db.Column(db.Date)  # 上映时间
+    length = db.Column(db.String(100))  # 播放时长
+    addtime = db.Column(db.DateTime, index=True, default=datetime.uncnow)  # 添加时间
+    comment = db.relationship('Comment', backref='movie')  # 评论外键关联
+    moviecols = db.relationship('Moviecol', backref='movie')  # 收藏外键关系关联
+
+    def __repr__(self):
+        return "<Movie %r>" % self.title
+
+
+# 上映预告
+class Preview(db.Model):
+    __tablename__ = 'preview'
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    logo = db.Column(db.String(255), unique=True)  # 封面
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return "<Preview %r>" % self.title
+
+
+# 评论
+class Comment(db.Model):
+    __tablename = 'comment'
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    content = db.Column(db.Text)  # 内容
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))  # 所属电影
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))  # 所属用户
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return '<Comment %r>' % self.id
+
+
+# 电影收藏
+class Moviecol(db.Model):
+    __tablename__ = 'moviecol'
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    content = db.Column(db.Text)  # 内容
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie_id'))  # 所属电影
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))  # 所属用户
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return "<Moviecol %r" % self.id
+
+
+# 权限
+class Auth(db.Model):
+    __tablename = 'auth'
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), unique=True)  # 名称
+    url = db.Column(db.String(255), unique=True)  # 地址
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加事假
+
+    def __repr__(self):
+        return '<Auth %r>' % self.name
+
+
+# 角色
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), unique=True)  # 名称
+    auths = db.Column(db.String(600))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+# 管理员
+
+# 管理员登录日志
+
+#操作日志
